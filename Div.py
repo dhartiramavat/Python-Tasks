0228ce3b0e6f017090386b58df75f54af36a764a
dhartiramavat <dhartiramavat@gmail.com>
2026-04-15 22:50:54 +0530

    Add files via upload

---
+++ Div.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 != 0:
+    result = num1 / num2
+    print(f"The result is: {result}")
+else:
+    print("Error: Cannot divide by zero.")
