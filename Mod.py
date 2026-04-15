37959eb653bcc6eef5bf2af134fb0265662bfc4f
dhartiramavat <dhartiramavat@gmail.com>
2026-04-15 22:51:20 +0530

    Add files via upload

---
+++ Mod.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+remainder = num1 % num2
+
+print(f"The remainder of {num1} divided by {num2} is {remainder}")
